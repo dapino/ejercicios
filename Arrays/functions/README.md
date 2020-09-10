f4c6e34b35fed610e2b96339eb2e14ee7037a3fb
Mayra <mayrarodriguez1709@gmail.com>
2020-09-10 09:04:20 -0500

    chore: setup project and  array

---
+++ Arrays/functions/README.md
@@ -0,0 +1,9 @@
+# Array Functions Exercise
+About this file please notice 
+that you must complete all 
+implementations Be attentive about 
+parameters, return values and exports
+
+## Resources
+- [Array functions](https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array)
+- [Table values ASCII](http://www.asciitable.com/)
---
+++ Arrays/functions/array.functions.js
@@ -0,0 +1,59 @@
+/**
+ *  * Add element in an array
+ * @param array
+ * @param element
+ * @returns any[]
+ */
+const add = (array, element) => array.push(element);
+
+/**
+ * Remove element in array
+ * @param array
+ * @param element
+ */
+const remove = (array, element) => {};
+
+/**
+ * Remove by specific position
+ * @param position
+ */
+const removeByPosition = (position) => {};
+
+/**
+ * Sort by function
+ * @param fn
+ */
+const sort = (fn) => {}
+
+/**
+ * Join with a second array
+ * @param newArray
+ */
+const joinWith = (newArray) => {}
+
+const searchBy = (value) => {}
+
+/**
+ * Return the array values
+ */
+const values = () => {};
+
+/**
+ * Get the array
+ * position numbers
+ */
+const size = () => {}
+
+/**
+ * Clear array
+ */
+const clear = () => {}
+
+/**
+ * Convert array to String
+ */
+const toString = () => {}
+
+
+
+export {add, remove, removeByPosition, values}
